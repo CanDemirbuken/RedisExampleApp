0d33e3292892a1b3c484f303c684d695b1efdbeb
Can <103042124+Canmellow@users.noreply.github.com>
2024-04-04 01:12:06 +0300

    Add project files.

---
+++ RedisExampleApp.API/Services/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using RedisExampleApp.API.Models;
+using RedisExampleApp.API.Repository;
+
+namespace RedisExampleApp.API.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Product> AddAsync(Product product)
+        {
+            await _productRepository.AddAsync(product);
+
+            return product;
+        }
+
+        public async Task<List<Product>> GetAsync()
+        {
+            return await _productRepository.GetAsync();
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _productRepository.GetByIdAsync(id);
+        }
+    }
+}
